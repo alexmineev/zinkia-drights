2d2625f11a52bcfe2411eff2be6244a0d78c86e5
alexey.mineev <alexey.mineev@5ba83a42-dac5-4a48-b111-e6d4d4672f85>
2016-05-30 13:39:34 +0000

    first commit

---
+++ glasscube/YTAPI/Videos.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Videos
+ *
+ * @author alexey.mineev
+ */
+class Videos {
+    public static function fromCSVReport(String $reportFile) {
+        
+    }
+    
+}
